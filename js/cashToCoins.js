7be7f2b4346330011e4526ba2bd744e6fca41da8
Million Yemane <wedi_ngus@yahoo.com>
2018-08-12 17:57:57 -0500

    go

--- js/cashToCoins.js
+++ js/cashToCoins.js
@@ -37,3 +37,64 @@ if (coinValue>=25){                           //if it is greater or equal 25
     //console.log(piggyBank.nickels);
 }
 console.log(piggyBank);
+var h12=document.getElementsByTagName('h1');
+console.log("h1", h12[0]);
+// .innerHTML=piggyBank.pennies;
+h12[0].style.color="black";
+
+var fruit = ["cherries", "apples", "bananas", "Man2go"];
+console.log(fruit);
+var fruits = "watermelon|lemon|orange";
+var fruitstr= fruit.sort().reverse();
+var spli=fruit.splice(4, 1,'million');
+var len =fruit.length;
+console.log("sort",fruitstr);
+console.log('splice',spli);
+var num =[10, 15, 13, 2, 3, 4, 5, 6]
+var nums=num.sort();
+var numb=num.sort(function(first, second){
+    return (first-second) 
+})
+// )
+console.log("nums",nums);
+fLen = fruit.length;
+console.log("flen",fLen);
+text = "<ul>";
+for (i = 0; i < fLen; i++) {
+    text += "<li>" + fruits[i] + "</li>";
+ 
+}
+console.log("i", i)
+var color=["red", "blue", "greeen", "million"];
+ var printcolor=color.join(",");
+ var slicecolor=color.slice(1 ,2);
+ var splitcolor=printcolor.split(' ');
+ var indexof=color.indexOf("million");
+ console.log("join",printcolor);
+//  console.log("slice", slicecolor);
+ console.log("split",splitcolor);
+//  console.log("indexof",indexof);
+
+
+splitcolor.forEach(function(color){
+    console.log("loop",color)
+})
+
+for(let i=0; i<splitcolor.length; i++){
+    console.log("forLoop",splitcolor[i])
+}
+ console.log("color",color);
+ var reversed=color.map(function (color) {
+    // console.log("map", color)
+    return color.split('').reverse().join('');
+})
+console.log("reversemap",reversed)
+
+var reversed=[];
+for (let i = 0; i < splitcolor.length; i++) {
+    console.log("reverseLoop", splitcolor[i])
+
+      var colorn=splitcolor[i].split('').reverse().join('');
+         reversed.push(colorn);
+}
+console.log("map", reversed)
