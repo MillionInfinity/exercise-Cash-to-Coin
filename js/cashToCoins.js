a163d98d4c8d9b3e372b3ce8a587c5537c565871
Million Yemane <wedi_ngus@yahoo.com>
2018-08-14 22:28:31 -0500

    all ex in one

--- js/cashToCoins.js
+++ js/cashToCoins.js
@@ -98,3 +98,148 @@ for (let i = 0; i < splitcolor.length; i++) {
          reversed.push(colorn);
 }
 console.log("map", reversed)
+
+var filtro=color.filter(function(color){
+    return color.length==4;
+})
+console.log("filter",filtro);
+var filters=[];
+for(var i=0; i<color.length; i++){
+        if(color[i].length===6){
+      filters.push(color[i])
+        }
+}
+    console.log("filters",filters);
+
+    var number=[51, 24, 10, 9, 6];
+
+    var sum=number.reduce(function(prev, curr){
+        return (prev + curr);
+    });
+    console.log("sum reduce", sum);
+
+    var sum1=0;
+    for(var i=0; i<number.length; i++){
+        sum1 += number[i];
+       }
+console.log("sum loop reduce", sum1);
+
+var sentence=color.filter(function(word){
+    return word.length>4;
+}).reduce(function(previous, current){
+    return previous +" " +current;
+});
+console.log("sentence",sentence);
+
+var solsys=(function(){
+    console.log("planets", solsys);
+    var plar=[];
+    var sol=0;
+    plar.push=("ear", "jup","mar");
+    sol.push=("sat","pla","ura");
+    return {
+        getPlanets:function(){
+            return plar;
+        }
+    }
+}
+)
+// console.log("planets",solsys);
+
+let bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+let vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+
+// Get a reference to the appropriate DOM element for bands
+const bandElement = document.getElementById('band');
+
+// Get a reference to the appropriate DOM element for vegetables
+// const veggieElement = document.getElementById('veggi');
+
+// Execute a for loop that will iterate over the arrays
+// for (let l = 0; l <bands.length; l += 1) {
+
+//     // Get a reference to the current item in the bands array
+//     const currentBand =[];
+//    console.log("band",currentBand);
+//     // Update the innerHTML value of the DOM element for bands
+
+//     // Get a reference to the current item in the vegetables array
+//     // const currentVeggie =
+
+//     // Update the innerHTML value of the DOM element for vegetables
+// };
+
+var currentba=[];
+for(i=0; i<bands.length; i++){
+    //  console.log("new band",bands[i]);
+     currentba.push(bands[i]);
+//  let bandi=currentba[i];
+//    currentba+=;
+   bandElement.innerHTML +='<p>'+currentba[i];
+}
+// console.log(currentba);
+
+var colores = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+
+var hohohoElement = document.getElementById("veggi");
+
+var rain='';
+for(i=0; i<reindeer.length; i++){
+    // console.log(reindeer[i]);
+    rain+='<h6>'+ colores[i]+ ","+ reindeer[i];
+    // console.log("rain",rain);
+    // rain.push(reindeer[i]);
+}
+hohohoElement.innerHTML+=rain;
+var value = 10;
+switch (value) {
+    case 1:
+        console.log("Small number");
+        break;
+    case 5:
+        console.log("Medium number");
+        break;
+    case 10:
+        console.log("Large number");
+        break;
+    default:
+        console.log("Dunno");
+}
+
+var today=new Date().getDate();
+console.log("the day of today",today);
+let day;
+switch(new Date().getDay()){
+    case 0:
+    day ="sunday";
+    break;
+    case 1:
+    day ="Monday";
+    break;
+    case 2:
+    day ="Tuesday";
+    break;
+    case 3:
+    day ="Wednsday";
+    break;
+    default:
+    console.log("you did not finish writing the days");
+}
+console.log(day);
+
+var animal="Dog";
+switch(animal){
+    case 'cow':
+    case 'giraffe':
+    case 'Dog':
+    console.log('this animal will go');
+    break;
+    case'spoon':
+    console.log("spoon is not an animal");
+    break;
+    default:
+    console.log("this animal will not be in the ark");
+}
+// console.log (animal);
